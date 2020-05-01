ee3f955f4f4ccb938affc61b1f06820df2d305eb
xwang2713 <xiaoming.wang@lexisnexis.com>
2020-04-30 23:57:15 -0400

    clean VM even build fails

--- template/bionic/build.sh
+++ template/bionic/build.sh
@@ -5,10 +5,12 @@ mkdir hld
 
 #rm -rf HPCCSystemsVM-amd64-7.0.18-1.ova
 
-vagrant up || exit 1
+vagrant up 
+rc=$?
 vagrant halt || exit 1
-vagrant package --output hpcc-bionic64.box
+[ $rc -eq 0 ] && vagrant package --output hpcc-bionic64.box
 vagrant destroy -f
+[ $rc -ne 0 ] && exit 1
 
 #cd hld
 #tar -xf HPCCSystemsVM-amd64-7.0.18-1.box
